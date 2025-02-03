6a7dc68bef357bcf6f802cc9ccff62c6a01a0cf7
massarovictor <30939659+massarovictor@users.noreply.github.com>
2025-02-03 18:26:46 -0300

    Update __init__.py

--- components/__init__.py
+++ components/__init__.py
@@ -1,1 +1,6 @@
+from .loading import loading_spinner
+from .dialogs import confirm_dialog
+from .handlers import error_handler, success_message
+
+__all__ = ['loading_spinner', 'confirm_dialog', 'error_handler', 'success_message']
 
